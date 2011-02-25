ffb60c2e331080fc0c414c640c4c18378dd0c679
Anthony Long <antlong@gmail.com>
2011-02-25 01:07:19 -0500

    Automatic commit.

--- selenium/selenium.py
+++ selenium/selenium.py
@@ -1,25 +1,7 @@
-"""
-Copyright 2006 ThoughtWorks, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Modified by Anthony Long, 2011.
-"""
-__docformat__ = "restructuredtext en"
-
 import httplib
 import urllib
 import time
+__docformat__ = "restructuredtext en"
 
 class selenium(object):
     def __init__(self, host, port, browserStartCommand, browserURL):
@@ -31,7 +13,6 @@ class selenium(object):
         self.extensionJs = ""
         self.headers = {"Content-Type":
                 "application/x-www-form-urlencoded; charset=utf-8"}
-        self.on_error = None
     
     def setExtensionJs(self, extensionJs):
         self.extensionJs = extensionJs
