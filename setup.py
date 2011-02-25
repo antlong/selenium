e75ea9cf0c0acf069bd99992c697a6dc28ff15ca
Anthony Long <antlong@gmail.com>
2011-02-24 21:41:02 -0500

    Automatic commit.

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        'Development Status :: 5 - Mature',
         'Intended Audience :: Developers',
         'Programming Language :: Python', ],
 )
